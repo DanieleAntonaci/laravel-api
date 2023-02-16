2b22c27639a89a1d831246f73424b50c52e3a43e
DanieleAntonaci <danieleanto96@gmail.com>
2023-02-16 16:26:17 +0100

    Stampa in console del file axios

---
+++ front-end/src/App.vue
@@ -0,0 +1,27 @@
+<script >
+import axios from 'axios'
+export default {
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/v1/movies')
+      .then(res => {
+        const data = res.data;
+        const movie = data.responde;
+        console.log(movie);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  },
+
+}
+</script>
+
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<style scoped>
+
+</style>
